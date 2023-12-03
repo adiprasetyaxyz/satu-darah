06ac5c38f5b3fcca6c64684116c1d735ee4f6163
unknown <adiprasetyaxyz@icloud.com>
2023-12-04 00:30:31 +0800

    Style: membuat navbar dan setup pages

--- front-end/src/scripts/index.js
+++ front-end/src/scripts/index.js
@@ -1,4 +1,18 @@
-// import React from 'react';
-// import { createRoot } from 'react-dom';
-// import App from './App';
 import '../styles/style.scss';
+// eslint-disable-next-line import/no-unresolved
+import App from './views/App';
+
+// eslint-disable-next-line no-unused-vars
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
