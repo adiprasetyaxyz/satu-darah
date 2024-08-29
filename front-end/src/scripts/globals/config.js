1c59992d97125210dfb1f9037d542e6a3b3710e4
unknown <adiprasetyaxyz@icloud.com>
2024-08-30 00:26:45 +0800

    change Backend link

--- front-end/src/scripts/globals/config.js
+++ front-end/src/scripts/globals/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
-  BASE_URL: 'https://good-gold-vulture-wig.cyclic.app/',
-  BASE_IMAGE_URL: '',
-  DEFAULT_LANGUAGE: 'en-us',
+  KEY: "YOUR_API_KEY",
+  BASE_URL: "https://satu-darah.adaptable.app/",
+  BASE_IMAGE_URL: "",
+  DEFAULT_LANGUAGE: "en-us",
 };
 
 export default CONFIG;
